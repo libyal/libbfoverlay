11b62396c63e0208cef9f0427545c20d5de85e9a
Joachim Metz <joachim.metz@gmail.com>
2021-03-31 06:15:52 +0200

    Worked on tests

---
+++ tests/bfoverlay_test_tools_signal.c
@@ -0,0 +1,213 @@
+/*
+ * Tools signal functions test program
+ *
+ * Copyright (C) 2020-2021, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <file_stream.h>
+#include <types.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( WINAPI )
+#include <stdlib.h>
+#endif
+
+#include "bfoverlay_test_libcerror.h"
+#include "bfoverlay_test_macros.h"
+#include "bfoverlay_test_unused.h"
+
+#include "../bfoverlaytools/bfoverlaytools_signal.h"
+
+void bfoverlay_test_tools_signal_handler_function(
+      bfoverlaytools_signal_t signal BFOVERLAY_TEST_ATTRIBUTE_UNUSED )
+{
+	BFOVERLAY_TEST_UNREFERENCED_PARAMETER( signal )
+}
+
+#if defined( WINAPI )
+
+/* Tests the bfoverlaytools_signal_handler function
+ * Returns 1 if successful or 0 if not
+ */
+int bfoverlay_test_tools_signal_handler(
+     void )
+{
+	BOOL result = 0;
+
+	/* Test regular cases
+	 */
+	result = bfoverlaytools_signal_handler(
+	          CTRL_C_EVENT );
+
+	BFOVERLAY_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 (int) TRUE );
+
+	result = bfoverlaytools_signal_handler(
+	          CTRL_LOGOFF_EVENT );
+
+	BFOVERLAY_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 (int) FALSE );
+
+	return( 1 );
+
+on_error:
+	return( 0 );
+}
+
+#if defined( _MSC_VER )
+
+	/* TODO add tests for bfoverlaytools_signal_initialize_memory_debug */
+
+#endif /* defined( _MSC_VER ) */
+
+#endif /* defined( WINAPI ) */
+
+/* Tests the bfoverlaytools_signal_attach function
+ * Returns 1 if successful or 0 if not
+ */
+int bfoverlay_test_tools_signal_attach(
+     void )
+{
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
+	 */
+	result = bfoverlaytools_signal_attach(
+	          bfoverlay_test_tools_signal_handler_function,
+	          &error );
+
+	BFOVERLAY_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	BFOVERLAY_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = bfoverlaytools_signal_attach(
+	          NULL,
+	          &error );
+
+	BFOVERLAY_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	BFOVERLAY_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
+}
+
+/* Tests the bfoverlaytools_signal_detach function
+ * Returns 1 if successful or 0 if not
+ */
+int bfoverlay_test_tools_signal_detach(
+     void )
+{
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
+	 */
+	result = bfoverlaytools_signal_detach(
+	          &error );
+
+	BFOVERLAY_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	BFOVERLAY_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
+}
+
+/* The main program
+ */
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+int wmain(
+     int argc BFOVERLAY_TEST_ATTRIBUTE_UNUSED,
+     wchar_t * const argv[] BFOVERLAY_TEST_ATTRIBUTE_UNUSED )
+#else
+int main(
+     int argc BFOVERLAY_TEST_ATTRIBUTE_UNUSED,
+     char * const argv[] BFOVERLAY_TEST_ATTRIBUTE_UNUSED )
+#endif
+{
+	BFOVERLAY_TEST_UNREFERENCED_PARAMETER( argc )
+	BFOVERLAY_TEST_UNREFERENCED_PARAMETER( argv )
+
+#if defined( WINAPI )
+
+	BFOVERLAY_TEST_RUN(
+	 "bfoverlaytools_signal_handler",
+	 bfoverlay_test_tools_signal_handler )
+
+#if defined( _MSC_VER )
+
+	/* TODO add tests for bfoverlaytools_signal_initialize_memory_debug */
+
+#endif /* defined( _MSC_VER ) */
+
+#endif /* defined( WINAPI ) */
+
+	BFOVERLAY_TEST_RUN(
+	 "bfoverlaytools_signal_attach",
+	 bfoverlay_test_tools_signal_attach )
+
+	BFOVERLAY_TEST_RUN(
+	 "bfoverlaytools_signal_detach",
+	 bfoverlay_test_tools_signal_detach )
+
+	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
+}
+
